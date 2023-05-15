38a405b79fadebd90148a36cdd1789e97c0fb2ca
anshitmishraa <anshitmishraa@gmail.com>
2023-05-15 22:35:05 +0530

    Added Dummy Data for Testing

--- src/main/java/com/project/VendorHub/VendorHubApplication.java
+++ src/main/java/com/project/VendorHub/VendorHubApplication.java
@@ -1,8 +1,11 @@
 package com.project.VendorHub;
 
 import com.project.VendorHub.constants.Constants;
+import com.project.VendorHub.data.seeding.DummyVendorDataSeeder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
@@ -10,6 +13,13 @@ public class VendorHubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendorHubApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner dataSeeder(DummyVendorDataSeeder dataSeeder) {
+		return args -> {
+			dataSeeder.seedDummyVendors();
+		};
 	}
 
 }
